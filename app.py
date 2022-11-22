2fd158b5a2441347b3ea411c23b96f5be04de535
Alex <blackwellag@appstate.edu>
2022-11-21 19:59:39 -0500

    cant get mcstatus to work

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from mcstatus import JavaServer
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/api')
 def index():
-    return "hello world"
-
+    # server = JavaServer.lookup("ringley.us")
+    # status = server.status()
+    # return f"The server has {status.players.online} player(s) online and replied in {status.latency} ms"
+    print('yes')
+    return "no"
 
 if __name__ == '__main__':
     app.run(debug=True)
